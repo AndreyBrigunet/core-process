90038c9da7123afccb9ab7d3a363fe3ba504883d
Andrei Brigunet <andreybrigunet@gmail.com>
2024-12-03 02:57:33 +0200

    v1

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import os
 import time
+import re
+from copy import deepcopy
 from enum import Enum
 from jsonmerge import merge
 
@@ -7,36 +9,81 @@ from core_client import Client
 from core_client.base.models.v3 import (
     ProcessConfig,
     ProcessConfigLimit,
-    ProcessConfigIO
+    ProcessConfigIO,
+    ProcessConfigIOCleanup
 )
+
+# Local
+from dotenv import load_dotenv
+load_dotenv()
 
 CORE_ADDRESS = os.getenv('CORE_ADDRESS', '')
 CORE_USERNAME = os.getenv('CORE_USERNAME', '')
 CORE_PASSWORD = os.getenv('CORE_PASSWORD', '')
 
-PROCESS_REFERENCE = os.getenv('PROCESS_REFERENCE', 'streamfile')
-PROCESS_OUTPUT_PROTOCOL = os.getenv('OUTPUT_PROTOCOL', 'hls')
-PROCESS_OUTPUT_PROTOCOL = PROCESS_OUTPUT_PROTOCOL.split(",")
-
-STREAMFILE_FOLDER = os.getenv('STREAMFILE_FOLDER', './streamfiles')
+PROCESS_REFERENCE = os.getenv('PROCESS_REFERENCE', 'rtmp:hls')
 SYNC_INTERVAL_SECONDS = int(os.getenv('SYNC_INTERVAL_SECONDS', 10))
 
 process_config = ProcessConfig(
     id="dummy",
     reference=PROCESS_REFERENCE,
-    options=["-loglevel", "info"],
+    options=["-loglevel", "level+info", "-err_detect", "ignore_err", "-y"],
     input=[
         ProcessConfigIO(
             address="dummy",
-            id="0",
-            options=["-re"]
+            id="input_0",
+            options=[            
+                "-fflags", "+genpts",
+                "-thread_queue_size", "512",
+                "-probesize", "5000000",
+                "-analyzeduration", "3000000",
+                "-rtmp_enhanced_codecs", "hvc1,av01,vp09"
+            ]
         )
     ],
     output=[
         ProcessConfigIO(
-            address="{srt,name=dummy}",
-            id="0",
-            options=["-c", "copy", "-f", "hls"]
+            address="{memfs}/{processid}_{outputid}.m3u8",
+            id="output_0",
+            options=[
+                "-dn", "-sn",
+                "-map", "0:1",
+                "-codec:v", "copy",
+                "-map", "0:0",
+                "-codec:a", "copy",
+                "-f","hls",
+                "-start_number","0",
+                "-hls_time","6",
+                "-hls_list_size","6",
+                "-hls_flags","append_list+delete_segments+program_date_time+temp_file",
+                "-hls_delete_threshold","4",
+                "-hls_segment_filename","{memfs}/{processid}_{outputid}_%04d.ts",
+                "-master_pl_name","{processid}.m3u8",
+                "-master_pl_publish_rate","2",
+                "-method","PUT"
+            ],
+            cleanup=[
+                ProcessConfigIOCleanup(
+                    pattern="memfs:/{processid}**",
+                    purge_on_delete=True
+                ),
+                ProcessConfigIOCleanup(
+                    pattern="memfs:/{processid}_{outputid}.m3u8",
+                    max_file_age_seconds=60,
+                    purge_on_delete=True
+                ),
+                ProcessConfigIOCleanup(
+                    pattern="memfs:/{processid}_{outputid}_**.ts",
+                    max_files=24,
+                    max_file_age_seconds=60,
+                    purge_on_delete=True
+                ),
+                ProcessConfigIOCleanup(
+                    pattern="memfs:/{processid}.m3u8",
+                    max_file_age_seconds=60,
+                    purge_on_delete=True
+                )
+            ]
         )
     ],
     limits=ProcessConfigLimit(
@@ -44,137 +91,76 @@ process_config = ProcessConfig(
         memory_mbytes=0,
         waitfor_seconds=0
     ),
+    autostart=True,
     reconnect=True,
     reconnect_delay_seconds=2,
     stale_timeout_seconds=20,
 )
 
-
-def config_is_uptodate(file_process_config: dict, core_process_config: dict):
+def is_valid_stream_key(value: str):
     """ """
-    compare_config = merge(core_process_config, file_process_config)
+    pattern = r"^[A-Z]{4}-[a-z0-9]{6}$"
+    return bool(re.match(pattern, value))
+
+
+def config_is_uptodate(rtmp_process_config: dict, core_process_config: dict):
+    """ """
+    compare_config = merge(core_process_config, rtmp_process_config)
     if compare_config == core_process_config:
         return False
     return True
 
 
-def set_input_file_output(stream_name: str, hls_path: str = ""):
-    """generated output configs for a process"""
-    hls = ProcessConfigIO(
-        id="0",
-        address=f"{{memfs}}/{hls_path}{stream_name}.m3u8",
-        options=["-c", "copy", "-f", "hls"]
-    )
-    rtmp = ProcessConfigIO(
-        id="0",
-        address=f"{{rtmp,name={stream_name}}}",
-        options=["-c", "copy", "-f", "flv"]
-    )
-    srt = ProcessConfigIO(
-        id="0",
-        address=f"{{srt,mode=publish,name={stream_name}}}",
-        options=["-c", "copy", "-f", "mpegts"]
-    )
-    tee = ProcessConfigIO(
-        id="0",
-        address="",
-        options=["-map", "0", "-c", "copy", "-flags", "+global_header",
-                 "-tag:v", "7", "-tag:a", "10", "-f", "tee"]
-    )
-    available_outputs = {
-        "hls": hls,
-        "rtmp": rtmp,
-        "srt": srt
-    }
-    if len(PROCESS_OUTPUT_PROTOCOL) == 1:
-        output = available_outputs[PROCESS_OUTPUT_PROTOCOL[0]]
-    else:
-        output = tee
-        endpoint = []
-        if "hls" in PROCESS_OUTPUT_PROTOCOL:
-            endpoint.append(f"[f=hls]{hls.address}")
-        if "rtmp" in PROCESS_OUTPUT_PROTOCOL:
-            if len(endpoint) != 0:
-                endpoint.append("|")
-            endpoint.append(f"[f=rtmp]{rtmp.address}")
-        if "srt" in PROCESS_OUTPUT_PROTOCOL:
-            if len(endpoint) != 0:
-                endpoint.append("|")
-            endpoint.append(f"[f=mpegts]{srt.address}")
-        output.address = ''.join(endpoint)
-    return output
+def create_process_config():
+    """creates an process config for rtmp list."""
+    process_list = []
+    for core_rtmp in core_rtmp_list:
+        rtmp_name = core_rtmp.name.split('/')[-1]
+        
+        if is_valid_stream_key(rtmp_name):
+            config = deepcopy(process_config)
+            config.id = rtmp_name
+            config.input[0].address = f"{{rtmp,name={rtmp_name}}}"
+            process_list.append(config)
+
+    return process_list
 
 
-class FileProcessConfigType(Enum):
-    input = "input"
-    output = "output"
-
-
-def create_file_process_config(
-    listdir: str, type: FileProcessConfigType
-):
-    """creates an process config for each stream file
-    and return all conifg as a list.
-
-    Args:
-        listdir (str): folder with stream files
-
-    Returns:
-        _type_: list of stream file configs
-    """
-    file_process_list = []
-    for stream_file in os.listdir(listdir):
-        if stream_file.split(".")[1] == "stream":
-            stream = open(f"{listdir}/{stream_file}", 'r')
-            for line in stream.readlines():
-                file_process_config = process_config
-                file_process_config.id = stream_file.split(".")[0]
-                file_process_config.input[0].address = f"{line.strip()}"
-                if type == "input":
-                    file_process_config.output[0] = set_input_file_output(
-                        stream_name=stream_file.split(".")[0])
-                file_process_list.append(file_process_config)
-    return file_process_list
-
-
-def create_file_processes(file_process_list: list):
+def create_processes(rtmp_process_list: list):
     """creates or updates each stream file process
     on the assigned core url.
 
     Args:
-        file_process_list (list): list of stream file configs.
+        rtmp_process_list (list): list of stream file configs.
     """
-    for file_process in file_process_list:
+    for rtmp_process in rtmp_process_list:
         is_unknown = True
         for core_process in core_process_list:
-            if (file_process.id == core_process.id
-                    and core_process.reference == PROCESS_REFERENCE):
+            if (rtmp_process.id == core_process.id and core_process.reference == PROCESS_REFERENCE):
                 is_unknown = False
                 if config_is_uptodate(
-                    file_process_config=file_process.dict(),
+                    rtmp_process_config=rtmp_process.dict(),
                     core_process_config=core_process.config.dict()
                 ):
-                    print(f'update process id "{file_process.id}"')
-                    client.v3_process_put(
-                        id=core_process.id, config=file_process)
+                    print(f'update process id "{rtmp_process.id}"')
+                    client.v3_process_put(id=core_process.id, config=rtmp_process)
         if is_unknown:
-            print(f'create process id "{file_process.id}"')
-            client.v3_process_post(config=file_process)
+            print(f'create process id "{rtmp_process.id}"')
+            client.v3_process_post(config=rtmp_process)
 
 
-def clear_core_processes(file_process_list: list):
+def clear_core_processes(rtmp_process_list: list):
     """removes all processes with PROCESS_REFERENCE
-    and if not exists in file_process_list.
+    and if not exists in rtmp_process_list.
 
     Args:
-        file_process_list (list): list of stream file configs.
+        rtmp_process_list (list): list of stream file configs.
     """
     for core_process in core_process_list:
         if core_process.reference == PROCESS_REFERENCE:
             is_unknown = True
-            for file_process in file_process_list:
-                if (file_process.id == core_process.id
-                        and core_process.reference == PROCESS_REFERENCE):
+            for rtmp_process in rtmp_process_list:
+                if (rtmp_process.id == core_process.id and core_process.reference == PROCESS_REFERENCE):
                     is_unknown = False
             if is_unknown:
                 print(f'delete process id "{core_process.id}"')
@@ -190,23 +176,42 @@ client = Client(
 client.login()
 
 # start the loop
-while True:
+try:
+    while True:
+        try:
+            input_process_list = []
+            start_time = time.time() 
+            core_rtmp_list = client.v3_rtmp_get()
+            end_time = time.time() 
+            execution_time = end_time - start_time
+            print(f"timpul executie: v3_rtmp_get: {execution_time:.2f} secunde")
+            # print(core_rtmp_list)
+            
+            # if core_rtmp_list:
+            start_time = time.time()
+            core_process_list = client.v3_process_get_list(reference=PROCESS_REFERENCE)
+            end_time = time.time() 
+            execution_time = end_time - start_time
+            print(f"timpul executie: v3_process_get_list: {execution_time:.2f} secunde")
 
-    try:
-        # fetch all running core processes
-        core_process_list = client.v3_process_get_list()
+            # print(core_process_list)
+        
+            # create a temp. list of stream file configs
+            input_process_list = create_process_config()
+            # print(input_process_list)
 
-        # create a temp. list of stream file configs
-        input_process_list = create_file_process_config(
-            listdir=STREAMFILE_FOLDER, type="input")
+            
+            # create or update stream file processes
+            create_processes(rtmp_process_list=input_process_list)
 
-        # create or update stream file processes
-        create_file_processes(file_process_list=input_process_list)
+            # remove dropped stream file on core
+            clear_core_processes(rtmp_process_list=input_process_list)
+        
+        except Exception as e:
+            print(f"error: {e}")
 
-        # remove dropped stream file on core
-        clear_core_processes(file_process_list=input_process_list)
-
-    except Exception as e:
-        print(f"error: {e}")
-
-    time.sleep(SYNC_INTERVAL_SECONDS)
+        time.sleep(SYNC_INTERVAL_SECONDS)
+except KeyboardInterrupt:
+    print("Run return except: KeyboardInterrupt")
+except SystemExit:
+    print("Run return except: SystemExit")
